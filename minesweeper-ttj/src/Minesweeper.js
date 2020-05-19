fc41b5e5be2159a421eb5296558670e889e2d318
Mete <meteabduraman@gmail.com>
2020-05-20 01:19:12 +0300

    preluare username + container joc

---
+++ minesweeper-ttj/src/Minesweeper.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './Minesweeper.css';
+
+class Minesweeper extends React.Component {
+    render() {
+        return (
+            <div className="container">well acum e acum</div>
+        )
+    }
+}
+
+function Render() {
+    return (
+        <React.Fragment>
+            <Minesweeper />
+        </React.Fragment>
+    )
+}
+
+export default Minesweeper;
