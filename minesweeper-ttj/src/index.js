838f47f1f56a2b2749f1cd615c29f03d595aff34
Ana-Maria Bordeianu <anamariabordeianu9@gmail.com>
2020-05-21 15:10:13 +0300

    TODO API GET+POST

--- minesweeper-ttj/src/index.js
+++ minesweeper-ttj/src/index.js
@@ -7,7 +7,7 @@ import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Minesweeper />
+    <User />
   </React.StrictMode>,
   document.getElementById('root')
 );
