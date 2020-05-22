3c1d43dbbf1e2c57bc8135b60b895c2a4ef00b04
Ana-Maria Bordeianu <anamariabordeianu9@gmail.com>
2020-05-22 11:51:24 +0300

    API done

---
+++ minesweeper-ttj/API/WebApplication1/Models/Player.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Models
+{
+    public class Player
+    {
+        public int ID { get; set; }
+        public string name { get; set; }
+        public int score { get; set; }
+        public DateTime time { get; set; }
+        public string Secret { get; set; }
+
+    }
+}
---
+++ minesweeper-ttj/API/WebApplication1/Models/PlayerDTO.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Models
+{
+    public class PlayerDTO
+    {
+        public int ID { get; set; }
+        public string name { get; set; }
+        public int score { get; set; }
+        public DateTime time { get; set; }
+    }
+}
